dbab4c1ec8040a556568ef422e2e35978d05cfbf
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 14:13:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,10 @@
  def prime? (int)
    rand_array = (2..Math.sqrt(int)).to_a
    rand_array.each do |x|
-     if int % x == 0 
-       false
-     else
+     if int % x != 0 
        true
+     else
+       false
      end
    end
  end
