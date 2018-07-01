6d1db2ee94b54816d3b661be1bb98f2e9f1c733a
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 15:00:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime? (int)
+
+is_prime = true
+
+(2..int-1).each do |x|
+     if int % x == 0 
+       is_prime=false
+     end 
+   end
+  
+  if int < 2 
+    is_prime=false
+  end
+   
+   is_prime
+ end
