12df676ba8e28d58e1470ab669c692ba7e6e4c0d
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 14:38:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
  
  def prime? (int)
+   if int < 2 
+      false
+    end
    (2..int-1).each do |x|
      if int % x == 0 
-       false
-       elsif int < 2 
-        false 
+       false 
      end 
    end
    true
