6d6647b7c618b77dd0d193f25b1fa5e707b78ce5
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 14:07:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
  
  def prime? (int)
-   rand_array = [2..Math.sqrt(int)]
+   rand_array = *(2..Math.sqrt(int))
    rand_array.each do |number|
      if int % number == 0 
        false
