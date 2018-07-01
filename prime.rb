1061b96672be466f128e0600d48727532a461ae1
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 14:45:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,17 @@
  def prime? (int)
    if int <= 1 
       false
-      "Be sure to account for negative numbers!"
     end
-    if int == 2 
+  
+  if int == 2 
       true
     end
-   (2..int-1).each do |x|
+   
+   (2..int).each do |x|
      if int % x == 0 
        false 
      end 
    end
+   
    true
  end
