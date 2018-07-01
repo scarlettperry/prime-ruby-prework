4a27340f31fed84b8075c0ce9d4f7c266c582a58
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 14:56:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 def prime? (int)
 is_prime = true
-  if int <= 1 
+  if int < 2 
       is_prime=false
     end
   
-  if int == 2 
-      is_prime=true
-    end
+  
     
   (2..int-1).each do |x|
      if int % x == 0 
